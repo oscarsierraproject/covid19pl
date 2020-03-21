oscarsierraproject/covid19pl

commit e1286a204cec97ca3cc99fa3fe406b2fab97e88f

REFACTOR: Removing mandatory options
Workspace is moved from mandatory options into optional ones.

covid19pl.py
@@ -4,7 +4,7 @@
 __author__      = "oscarsierraproject.eu"
 __copyright__   = "Copyright 2020, oscarsierraproject.eu"
 __license__     = "GNU General Public License 3.0"
-__version__     = "1.0.0"
+__version__     = "1.1.0"
 __date__        = "21st March 2020"
 __maintainer__  = "oscarsierraproject.eu"
 __email__       = "oscarsierraprojectk@protonmail.com"
@@ -63,19 +63,17 @@ def parse_options():
                                     version = "%prog {}".format(__version__),
                                     epilog = "{}, {}".format(__copyright__,
                                                              __license__))
-    group = optparse.OptionGroup(parser, "MANDATORY OPTIONS")
-    group.add_option(  "--workspace", action="store", 
-                        type="string", dest="workspace",
-                        default=os.path.join( os.path.dirname( os.path.abspath(__file__)), 
-                                             "data"),
-                        help="path to directory with data [default: %default]")
-    parser.add_option_group(group)
     group = optparse.OptionGroup(parser, "OPTIONAL OPTIONS")
     group.add_option(  "--debug", action="store_true", dest="debug",
                         help="Run script in debug mode")
     group.add_option(  "--display", action="store_true", dest="display",
                         help="Display latest data for Poland")
     group.add_option(  "--gather", action="store_true", dest="gather",
+    group.add_option(  "--workspace", action="store", 
+                        type="string", dest="workspace",
+                        default=os.path.join( os.path.dirname( os.path.abspath(__file__)), 
+                                             "data"),
+                        help="path to directory with data [default: %default]")
                         help="Use this option to gather latest data from gov.pl")
     parser.add_option_group(group)
     (options, args) = parser.parse_args()

commit e3a8e75332aad3d65016e1143f07306b6878bff2

FIX: Error introduced with e1286a204
Fix error introduced while moving mandatory options into
optional ones.

covid19pl.py
@@ -69,12 +69,12 @@ def parse_options():
     group.add_option(  "--display", action="store_true", dest="display",
                         help="Display latest data for Poland")
     group.add_option(  "--gather", action="store_true", dest="gather",
+                        help="Use this option to gather latest data from gov.pl")
     group.add_option(  "--workspace", action="store", 
                         type="string", dest="workspace",
                         default=os.path.join( os.path.dirname( os.path.abspath(__file__)), 
                                              "data"),
                         help="path to directory with data [default: %default]")
-                        help="Use this option to gather latest data from gov.pl")
     parser.add_option_group(group)
     (options, args) = parser.parse_args()
     if not options.workspace or not os.path.isdir(options.workspace):